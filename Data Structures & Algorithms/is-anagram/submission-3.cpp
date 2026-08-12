f103fe73eff73c5298e54615bc5c58daae8096ec
Raj <137254223+Rajendran2201@users.noreply.github.com>
2026-08-12 23:30:44 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) return false;
+        unordered_map<char, int> s1, s2;
+        for (char c : s) s1[c]++;
+        for (char c : t) s2[c]++;
+
+        return s1 == s2;
+    }
+};
